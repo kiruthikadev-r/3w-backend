9b4ba3cf42f5e4ed7376adbaa2d5f32bbf548360
kiruthi <priyakiruthi21@gmail.com>
2024-10-13 21:04:48 +0530

    cors adjust

--- index.js
+++ index.js
@@ -19,17 +19,11 @@ const app = express();
 const allowedOrigins = ['*']; 
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
+
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI, {
